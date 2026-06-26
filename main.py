743edc8512e16df74a3a39e60af08205410f20f2
tuantiphu1 <tuantiphu1@gmail.com>
2026-06-26 13:03:26 +0700

    main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import requests
-from datetime import datetime
 
 TOKEN = "8980796473:AAFY1OC7fRiJVfwp6lPdiv3qgm5Y35RoiXo"
 CHAT_ID = "6067033565"
 
-# API xổ số
-data = requests.get("https://xoso-api.vercel.app/api/mb").json()
+try:
+    r = requests.get("https://xoso-api.vercel.app/api/mb", timeout=10)
 
-db = data["data"]["special_prize"]
+    msg = f"STATUS={r.status_code}\n\n{r.text[:1000]}"
 
-today = datetime.now().strftime("%d/%m/%Y")
-
-msg = f"🎯 XSMB {today}\n\nGiải đặc biệt: {db}"
+except Exception as e:
+    msg = str(e)
 
 requests.get(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
