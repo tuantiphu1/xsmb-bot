0fa62ad2678dc366f708b8e70b2afe6cc3641b74
tuantiphu1 <tuantiphu1@gmail.com>
2026-06-26 13:08:46 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,12 @@ import requests
 TOKEN = "8980796473:AAFY1OC7fRiJVfwp6lPdiv3qgm5Y35RoiXo"
 CHAT_ID = "6067033565"
 
-try:
-    r = requests.get("https://xoso-api.vercel.app/api/mb", timeout=10)
-
-    msg = f"STATUS={r.status_code}\n\n{r.text[:1000]}"
-
-except Exception as e:
-    msg = str(e)
-
-requests.get(
-    f"https://api.telegram.org/bot{TOKEN}/sendMessage",
+r = requests.get(
+    f"https://api.telegram.org/bot8980796473:AAFY1OC7fRiJVfwp6lPdiv3qgm5Y35RoiXo/sendMessage",
     params={
         "chat_id": CHAT_ID,
-        "text": msg
+        "text": "DEBUG"
     }
 )
+
+print(r.text)
