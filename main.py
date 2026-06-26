dbcdcd26dade92bba1617ef366f80fd5a1effcc6
tuantiphu1 <tuantiphu1@gmail.com>
2026-06-26 12:45:29 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,9 @@
 import requests
-import re
 
 TOKEN = "8980796473:AAFY1OC7fRiJVfwp6lPdiv3qgm5Y35RoiXo"
 CHAT_ID = "6067033565"
 
-url = "https://xoso.com.vn/xsmb-xo-so-mien-bac.html"
-
-html = requests.get(
-    url,
-    headers={"User-Agent": "Mozilla/5.0"}
-).text
-
-# Tìm giải đặc biệt 5 số
-match = re.search(r'jackpot[^>]*>\s*([0-9]{5})', html)
-
-if match:
-    db = match.group(1)
-else:
-    db = "Không lấy được kết quả"
-
-msg = f"🎯 Giải đặc biệt XSMB hôm nay:\n{db}"
+msg = "Test gửi Telegram"
 
 requests.get(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
