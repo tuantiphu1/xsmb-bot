14d66d13ebfdf5916ae28e601961796729437b44
tuantiphu1 <tuantiphu1@gmail.com>
2026-06-26 12:52:51 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,17 @@
 import requests
+from datetime import datetime
 
 TOKEN = "8980796473:AAFY1OC7fRiJVfwp6lPdiv3qgm5Y35RoiXo"
 CHAT_ID = "6067033565"
 
-msg = "Test gửi Telegram"
+# API xổ số
+data = requests.get("https://xoso-api.vercel.app/api/mb").json()
+
+db = data["data"]["special_prize"]
+
+today = datetime.now().strftime("%d/%m/%Y")
+
+msg = f"🎯 XSMB {today}\n\nGiải đặc biệt: {db}"
 
 requests.get(
     f"https://api.telegram.org/bot{TOKEN}/sendMessage",
